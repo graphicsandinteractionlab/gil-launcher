2b98ed52fca8fef4b1e7f12794b496688a9ee6c7
Hartmut Seichter <hartmut@technotecture.com>
2020-02-25 23:56:25 +0100

    WiP

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
-
-from flask import Flask
-from jinja2 import Template
+from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
 
 
+def launch_browser():
+#    res = subprocess.run(['firefox','--kiosk','http://localhost:5000'], capture_output=True)
+#   print(res)
+    subprocess.Popen(['firefox','--kiosk','http://localhost:5000'])
 
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('view.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
 
+if __name__ == '__main__':
+    launch_browser()
+    app.run(debug=True)
